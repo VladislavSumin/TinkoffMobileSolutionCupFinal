536cd68e38a045487d964edbee28ddf61280f12f
Vladislav Sumin <vladislavsumin@mail.ru>
2023-04-22 13:31:57 +0300

    add card elevation

--- uikit/src/main/kotlin/ru/vs/uikit/atoms/UiCard.kt
+++ uikit/src/main/kotlin/ru/vs/uikit/atoms/UiCard.kt
@@ -1,9 +1,8 @@
 package ru.vs.uikit.atoms
 
+import android.animation.AnimatorInflater
 import android.content.Context
-import android.graphics.Color
 import android.util.AttributeSet
-import android.view.View
 import android.widget.FrameLayout
 import ru.vs.uikit.R
 
@@ -15,5 +14,11 @@ class UiCard @JvmOverloads constructor(
 ) : FrameLayout(context, attrs, defStyleAttr, defStyleRes) {
     init {
         background = context.getDrawable(R.drawable.uikit_card_background)
+
+        stateListAnimator = AnimatorInflater.loadStateListAnimator(context, R.animator.uikit_card_elevation)
+
+        context.obtainStyledAttributes(attrs, R.styleable.UiCard).use {
+            isEnabled = it.getBoolean(R.styleable.UiCard_android_enabled, true)
+        }
     }
 }
