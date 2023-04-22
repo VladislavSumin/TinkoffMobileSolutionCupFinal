aed2fb03f5a50dda038c36c051c98999db2a34d9
Vladislav Sumin <vladislavsumin@mail.ru>
2023-04-22 13:52:58 +0300

    finish work on header part 1

--- uikit/src/main/kotlin/ru/vs/uikit/components/UikitHeaderCard.kt
+++ uikit/src/main/kotlin/ru/vs/uikit/components/UikitHeaderCard.kt
@@ -2,6 +2,8 @@ package ru.vs.uikit.components
 
 import android.content.Context
 import android.util.AttributeSet
+import android.widget.ImageView
+import android.widget.TextView
 import ru.vs.uikit.R
 import ru.vs.uikit.atoms.UiKitCard
 
@@ -11,7 +13,26 @@ class UikitHeaderCard @JvmOverloads constructor(
     defStyleAttr: Int = 0,
     defStyleRes: Int = 0,
 ) : UiKitCard(context, attrs, defStyleAttr, defStyleRes) {
+
+    private val header: TextView
+    private val subheader: TextView
+    private val icon: ImageView
+
     init {
         inflate(context, R.layout.uikit_header_card, this)
+
+        header = findViewById(R.id.uikit_header_card_header)
+        subheader = findViewById(R.id.uikit_header_card_subheader)
+        icon = findViewById(R.id.uikit_header_card_icon)
+
+        context.obtainStyledAttributes(attrs, R.styleable.UikitHeaderCard).use {
+            val headerText = it.getText(R.styleable.UikitHeaderCard_headerText)
+            val subheaderText = it.getText(R.styleable.UikitHeaderCard_subheaderText)
+            val iconDrawale = it.getDrawable(R.styleable.UikitHeaderCard_icon)
+
+            header.text = headerText
+            subheader.text = subheaderText
+            icon.setImageDrawable(iconDrawale)
+        }
     }
 }
